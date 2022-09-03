6f0e9f2d2c776a114d683b5197abc444e93f040e
jhon.alejo.2882 <jhon.alejo.2882@gmail.com>
2022-09-03 13:51:33 -0500

    commit final

--- src/main.js
+++ src/main.js
@@ -4,8 +4,9 @@ const handOptions = {
     "scissors": "images/icon-scissors.svg"
   }
   
-  let SCORE = 0;
-  
+let SCORE = 0;
+
+
   const pickUserHand = (hand) => {
     let hands = document.querySelector(".hands");
     hands.style.display = "none";
@@ -78,3 +79,32 @@ const handOptions = {
     SCORE = newScore;
     document.querySelector(".score h1").innerText = newScore;
   }
+
+
+
+    // Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var rulesbutton = document.getElementById("buttonModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+rulesbutton.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
